b2a4069672fb6fc8e7c131a787ccc79cae348ef7
Zachary Day <zach@dayzpd.com>
2018-06-11 05:40:18 -0500

    Fixing shell command file paths.

--- app/classes/shell.py
+++ app/classes/shell.py
@@ -4,20 +4,20 @@ class Shell:
 
     @staticmethod
     def build(name, user, password):
-        subprocess.check_call(['app/resources/shell_scripts/build.sh', name, user, password])
+        subprocess.check_call(['./resources/shell_scripts/build.sh', name, user, password])
 
     @staticmethod
     def pull(name, user, password):
-        subprocess.check_call(['app/resources/shell_scripts/pull.sh', name, user, password])
+        subprocess.check_call(['./resources/shell_scripts/pull.sh', name, user, password])
 
     @staticmethod
     def remove(name, user, password):
-        subprocess.check_call(['app/resources/shell_scripts/remove.sh', name, user, password])
+        subprocess.check_call(['./resources/shell_scripts/remove.sh', name, user, password])
 
     @staticmethod
     def run(name, port, user, password):
-        subprocess.check_call(['app/resources/shell_scripts/run.sh', name, str(port), user, password])
+        subprocess.check_call(['./resources/shell_scripts/run.sh', name, str(port), user, password])
 
     @staticmethod
     def stop(name, user, password):
-        subprocess.check_call(['app/resources/shell_scripts/stop.sh', name, user, password])
+        subprocess.check_call(['./resources/shell_scripts/stop.sh', name, user, password])
